925b9361cbe020d0f4c69b19ad79ddbe68bbe100
lariane-guerra <sguerra.lariane@gmail.com>
2022-01-26 17:42:17 -0300

    Váriaveis e Tipos

---
+++ Sintaxe JS/aula8-sintaxe-e-operadores/aulavar.js
@@ -0,0 +1,13 @@
+/*var firstName = 'Joao'
+let lastName = 'Souza'
+
+if (firstName === 'Joao'){
+    var firstName = 'Pedro'
+    let lastName = 'Silva'
+    lastName = 'Guerra'
+    console.log(firstName,lastName)
+}
+
+console.log(firstName, lastName)*/
+const FIRST_NAME = "Lari"
+console.log(FIRST_NAME);
